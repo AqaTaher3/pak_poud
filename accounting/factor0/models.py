07127695be8425c0244e3c738e25ef2cf146e2dd
taher <aqa.taher3@gmail.com>
2023-12-31 16:53:27 +0330

    factor foroosh

--- accounting/factor0/models.py
+++ accounting/factor0/models.py
@@ -3,24 +3,34 @@ from customer0.models import ZiNaf, Moshtary
 
 
 class Tage(models.Model):
-    jens = models.CharField(max_length=16)
-    vazn = models.DecimalField(max_digits=10, decimal_places=2)
-    metraj = models.DecimalField(max_digits=10, decimal_places=2)
+    jens_parche = models.CharField(max_length=16, default='nil')
+    vazn = models.IntegerField()
+    metraj = models.IntegerField(default='120')
     class Meta:
         ordering = ['-vazn', ]
 
     def __str__(self) -> str:
-        return self.vazn+self.jens
+        return self.vazn
 
 
 class FactorForoosh(models.Model):
     kharidar = models.ManyToManyField(Moshtary)
     tage_ha = models.ManyToManyField(Tage)
-    fee = models.DecimalField(max_digits=9, decimal_places=0)
+    fee = models.IntegerField()
     tarikhe_foroosh = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
+    Jame_kol = models.IntegerField(null=True, blank=True)  # اضافه کردن فیلد Jame_kol
+
+    def jame_factor(self):
+        total = sum(tage.vazn for tage in self.tage_ha.all())
+        return total * self.fee
+
+    def save(self, *args, **kwargs):
+        self.Jame_kol = self.jame_factor()  # محاسبه و ذخیره‌ی مقدار jame_factor در فیلد Jame_kol
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ['-tarikhe_foroosh', ]
 
     def __str__(self) -> str:
-        return self.kharidar[0:50] + self.tarikhe_foroosh
+        return str(self.kharidar.first()) + ' ' + str(self.tarikhe_foroosh)
