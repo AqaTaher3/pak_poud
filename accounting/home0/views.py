f6030893c2cecf45914fe6844894f9c667ce4a14
taher <aqa.taher3@gmail.com>
2024-01-02 18:03:54 +0330

    upgrade some

--- accounting/home0/views.py
+++ accounting/home0/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from customer0.models import Moshtary
 from chek0.models import Chek
+from factor0.models import Tage, Foroosh
 from django.db.models import Q
 
 
@@ -13,6 +14,9 @@ def home(request):
 
     kole_moshtari_ha = Moshtary.objects.all()
     customer_count = Moshtary.objects.all().count()
-    tede_moshtari = 15
-    content = ({'kole_moshtari_ha': kole_moshtari_ha, 'tede_moshtari':customer_count})
+    chek_be_nam_nashode = Chek.objects.filter(Q(ki_daryaft_kard__exact='') | Q(ki_daryaft_kard__isnull=True)).count()
+    magsad_nashode = Chek.objects.filter(Q(magsad__isnull=False) | Q(ki_daryaft_kard__exact='') 
+                                              | Q(ki_daryaft_kard__isnull=True)).count()
+    content = ({'kole_moshtari_ha': kole_moshtari_ha, 'tede_moshtari':customer_count,
+                'chek_be_nam_nashode':chek_be_nam_nashode, 'magsad_nashode':magsad_nashode})
     return render(request, 'home0/home.html', content)
